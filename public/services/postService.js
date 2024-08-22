6e210e1d81116230a78abaa6056448b4a8645ea9
AhlawatAbhishek <abhishekjobsid@pm.me>
2024-08-22 23:13:38 +0530

    refactor: Update postService to use axios for API requests

--- public/services/postService.js
+++ public/services/postService.js
@@ -1,1 +1,9 @@
 import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://jsonplaceholder.typicode.com",
+});
+
+const getPosts = () => api.get("/posts");
+
+export default { getPosts };
