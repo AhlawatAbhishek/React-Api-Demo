99afdbe1bef299725eb8ead8a56cc4c566e9ea2f
AhlawatAbhishek <abhishekjobsid@pm.me>
2024-08-25 22:40:10 +0530

    chore: Update npm dependencies and add @babel/plugin-proposal-private-property-in-object

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -6,11 +6,28 @@ export default function Posts() {
   useEffect(() => {
     getPosts()
       .then((response) => {
-        setPosts(response.data);
+        const data = response.data;
+        console.log(data);
+        setPosts(data);
       })
       .catch((error) => {
         console.error(error);
       });
   }, []);
-  return <></>;
+  return (
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => {
+          return (
+            <li key={post.id}>
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </li>
+          );
+        })}
+        <li></li>
+      </ul>
+    </div>
+  );
 }
